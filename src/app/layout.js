28446afa259cc3dd11e3b2aa135413fcb6707dc7
lakshit2808 <lakshitkanwal@outlook.com>
2023-08-13 08:53:06 +0530

    debug Services

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Head from 'next/head';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -12,6 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
